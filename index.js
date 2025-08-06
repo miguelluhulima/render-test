1a562ef530214cbe8cc5a2efcafa975f1e9b83c0
miguelluhulima <miguelluhu5@gmail.com>
2025-08-06 16:50:41 +0700

    added front end dist

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
